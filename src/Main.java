b3acc7f4fac25110f3b032882e71b7954d09a6e0
26zahra <zahra.abdulrahimova2021@khazar.org>
2024-05-11 20:42:00 +0400

    calculating factorial

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int a = 5;
-        int b = 10;
-
-        int temp = a + b;
-
-        System.out.println(temp);
+        int result =1;
+        int n =10;
+        // Multiply result by numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println(result);
     }
 }
